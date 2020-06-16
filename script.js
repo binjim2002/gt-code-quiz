04e17914ab4dec0e69a598c538c3875474ab7e0d
Jimmy B. Kazadi <binjim2002@yahoo.fr>
2020-06-16 13:44:24 -0700

    coding quiz adjustments

--- script.js
+++ script.js
@@ -1,110 +1,168 @@
-var headingEl = document.getElementById("heading");
-var pEl = document.getElementById("mainParagraph");
-var timerEl = document.getElementById("timer");
-var secondsLeft = 60;
-var startButtonEl = document.getElementById("start-button");
-var mainEl = document.getElementById("mainElement");
-var answer1El = document.getElementById("answer1");
-var answer2El = document.getElementById("answer2");
-var answer3El = document.getElementById("answer3");
-var answer4El = document.getElementById("answer4");
-
-headingEl.textContent = "Coding Quiz test";
-
-function countdownTimer() {
-    console.log("countdown Timer")
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        timerEl.textContent = "Time: " + secondsLeft;
-
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            console.log("timer up");
-
-        }
-    }, 1000);
-}
-startButtonEl.addEventListener("click", countdownTimer);
-// // startButtonEl.addEventlistener("click", nextQuestion);
-
+var timeEl = document.querySelector("time");
+var mainEl = document.getElementById("main");
+var startButton = document.getElementById("startButton");
+var timer = document.getElementById("remaining-time");
+var responses = document.getElementById("answers");
+var quizscreen = document.getElementById("quiz-screen");
+var questionsArray =  [{
+  question: "Commonly used data types DO NOT include:",
+  answers: ["strings", "booleans", "strings", "alerts"],
+  rightAnswer: "strings",
+},
+{
+  question: "The condition in an if/else statement is enclosed within:",
+  answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+  rightAnswer: "curly brackets",
+}, {
+  question: "Arrays in Javscript can be use to store:",
+  answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+  rightAnswer: "all of the above",
+}, {
+  question: "String values must be enclosed within ______ when being assigned to variables.",
+  answers: ["commas", "curly brackets", "quotes", "parentheses"],
+  rightAnswer: "quotes",
+},  {
+  question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+  answers: ["Javascript", "terminal/bash", "for loops", "console log"],
+  rightAnswer: "console log",
+}];
 var currentQuestionIndex = 0;
-
-var arrayOfQuestions = [
-    {
-        question: "Commonly used dat types DO NOT include",
-        answer1: "1. Strings",
-        answer2: "2. Booleans",
-        answer3: "3. Alerts",
-        answer4: "4. Numbers",
-    },
-    {
-        question: " The condition in an if/else statement is encoled within",
-        answer1: "1.Quotes",
-        answer2: "2.Curly braces",
-        answer3: "3. Parenthesis",
-        answer4: "4. Square Brackets",
-
-    },
-    {
-        question: "string values must be enclosed within_______when bein assigned to variables",
-        answer1: "1. Commas",
-        answer2: "2. Curly Braces",
-        answer3: "3. quotes",
-        answer4: "4. Paranthesis",
-    },
-    {
-        question: "Arrays in JavaScript can be used to store ____",
-        answer1: "1. Numbers and strings",
-        answer2: "2. Other Arrays",
-        answer3: "3. Booleans",
-        answer4: "4. All of the above",
-    },
-    {
-        question: "A very useful tool used during developpment and debugging for printing content to the browser",
-        answer1: "1. JavaScript",
-        answer2: "2.Terminal / bash",
-        answer3: "3. For loops",
-        answer4: "4. Console.log",
-
-    },
-];
-function nextQuestion() {
-    var currentQuestion = arrayOfQuestions[currentQuestionIndex]
-    mainEl.textContent = "";
-    var questionEl = document.createElement("h2");
-    questionEl.textContent = CurrentQuestion.question;
-    var answer1El = document.createElement("button");
-    answer1El.textContent.currentQuestion.answer1;
-    var answer2El = document.createElement("button");
-    answer2El.textContent = currentQuestion.answer2;
-    var answer3El = document.createElement("button");
-    answer3El.textContent = currentQuestion.answer3;
-    var answer4El = document.createElement("button");
-    answer4El.textContent = currentQuestion.answer4;
-
-    mainEl.append(questionEl);
-    mainEl.append(answer1El);
-    mainEl.append(answer2El);
-    mainEl.append(answer3El);
-    mainEl.append(answer4El);
+var userScore = 0
+;
+function startTime() {
+  var secondsLeft = "75";
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timer.textContent = secondsLeft + " seconds left";
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      quizscreen.style = "display: none";
+        }
+  }, 1000);
 }
-
-answer1El.addEventListener("click", function () {
-    console.log("clicked")
-    currentQuestionIndex++
-    nextQuestion();
-
-});
-answer2El.addEventListener("click", function () {
-    console.log("clicked")
-    currentQuestionIndex++
-    nextQuestion();
-
-    answer3El.addEventListener("click", function () {
-        console.log("clicked")
-        currentQuestionIndex++
-        nextQuestion();
-
-
+// function sendMessage() {
+//   timeEl.textContent = " ";
+//   var imgEl = document.createElement("img");
+//   imgEl.setAttribute("src", "images/image_1.jpg");
+//   mainEl.appendChild(imgEl);
+function displayQuestions() {
+var currentQuestion = questionsArray[currentQuestionIndex];
+startButton.remove();
+quizscreen.innerHTML = "";
+var newEl=document.createElement("h4");
+newEl.textContent = currentQuestion.question;
+quizscreen.append(newEl);
+// console.log(newEl);
+// var newAnswers = document.createElement("answerButtons");
+// newAnswers.textContent=questionsArray.Answers;
+// quizscreen.append(newAnswers);
+// console.log(newAnswers);
+for (var i = 0; i < currentQuestion.answers.length; i++)    {
+    var answersElement = document.createElement("button");
+    answersElement.setAttribute("class", "answers-i");
+    answersElement.setAttribute("class", "button btn-secondary");
+    answersElement.setAttribute("data-index", i);
+   
+    var quizAnswers = currentQuestion.answers[i];
+    answersElement.textContent = quizAnswers;
+   
+    quizscreen.append(answersElement);
+ 
+    answersElement.addEventListener("click", function(event) {
+      currentQuestionIndex++;
+      console.log(event.target);
+      displayQuestions();
+      var userchoice = event.target.textContent;
+      if  (userchoice === currentQuestion.rightAnswer)  {
+        alert("correct!");
+      userScore++;
+      } else {
+        secondsLeft=secondsLeft-10;
+        alert("incorrect!");
+        
+      }
+       console.log(userScore);
     })
-})
+//   if (questionsArray[0].rightAnswer);
+//   alert("correct!")
+// }else {
+//   alert("incorrect!")
+// }
+}}
+// document.getElementsById("quiz-screen").innerhtml="";
+// document.getElementById("startButton").addEventListener("click", function(){})
+// function nextQuestion()    {
+// nextQ = questionsArray[currentQuestionIndex[1]];
+// newEl1.textContent = 
+// }
+startButton.addEventListener("click", function() {
+displayQuestions();
+startTime()
+});
+// quizAnswers.addEventListener("click", nextQuestion())
+// How I want the quiz to work:
+// When I hit the "Start"  button , I want it to start the timer and enter the slide for the first question. 
+// When the first question is answered, I want it to record if the question was correct and move to the next question with a for loop.  
+// The for loop will cycle through the questions until it reaches the end .Answers
+// At this point the score will be presented and recorded in localStorage to later display . 
+// // The following animation demonstrates the application functionality:
+// var timeEl = document.querySelector(".time");
+// var mainEl = document.getElementById("main");
+// var startButton = document.getElementById("startButton");
+// var timer = document.getElementById("remaining-time");
+// var jumbotron = document.getElementById("jumbotron");
+// var questionsArray =  [{
+//   question: "Commonly used data types DO NOT include",
+//   Answers: ["strings", "booleans", "strings", "alerts"],
+//   rightAnswer: "strings",
+// },
+// {
+//   question: "The condition in an if/else statement is enclosed within:",
+//   firstAnswer: ["quotes", "curly brackets", "parentheses", "square brackets"],
+//   rightAnswer1: "curly brackets",
+// }, {
+//   question: "Arrays in Javscript can be use to store:",
+//   firstAnswer: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+//   rightAnswer2: "all of the above",
+// }, {
+//   question: "String values must be enclosed within ______ when being assigned to variables.",
+//   firstAnswer: ["commas", "curly brackets", "quotes", "parentheses"],
+//   rightAnswer3: "quotes",
+// },  {
+//   question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+//   fourthAnswers: ["Javascript", "terminal/bash", "for loops", "console log"],
+//   rightAnswere4: "console log",
+// }]
+// var currentQuestionIndex = 0
+// var secondsLeft = 75;
+// function setTime() {
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timer.textContent = secondsLeft + " seconds left";
+//     if(secondsLeft === 0) {
+//       clearInterval(timerInterval);
+//       sendMessage("You are out of time!");
+//     }
+//   }, 1000);
+// }
+// setTime();
+// // function sendMessage() {
+// //   timeEl.textContent = " ";
+// //   var imgEl = document.createElement("img");
+// //   imgEl.setAttribute("src", "images/image_1.jpg");
+// //   mainEl.appendChild(imgEl);
+// function displayQuestions() {
+    
+// var questionDisplayed = questionsArray[currentQuestionIndex];
+// jumbotron.textContent = "";
+// var h4El=document.createElement("h4");
+// h4El.textContent = questionDisplayed.question;
+// jumbotron.append(h4El);
+// // document.getElementsById("quiz-screen").innerhtml="";
+// // document.getElementById("startButton").addEventListener("click", function(){})
+// }
+// displayQuestions();
+// function nextQ()    {
+// }
+// startButton.addEventListener("click", displayQuestions);
+// startButton.addEventListener("click", setTime); 
